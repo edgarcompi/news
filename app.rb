3ec2f6f64f29e26300563c2d9a5eb543c6b4e382
edgarcompi <blancoandcompany@gmail.com>
2020-05-18 18:41:58 +0000

    WIP 2nd update

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-#require "sinatra"
-#require "sinatra/reloader"
+require "sinatra"
+require "sinatra/reloader"
 require "httparty"
-# def view(template); erb template.to_sym; end
+def view(template); erb template.to_sym; end
 
-# get "/" do
+ get "/" do
   ### Get the weather
   # Evanston, Kellogg Global Hub... replace with a different location if you want
   lat = 42.0574063
@@ -13,20 +13,19 @@ require "httparty"
   key = "83ec5ec6e6381230f14912693e7494e8" # replace this with your real OpenWeather API key
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
-  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
- 
+   url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
 
   # make the call
   @forecast = HTTParty.get(url).parsed_response.to_hash
 
-  day_number = 1
+  @day_number = 1
 
-puts "It is currently #{@forecast["current"]["temp"]} degrees and there is #{@forecast["current"]["weather"][0]["description"]}"
-puts "Extended forecast:"
-    for day in @forecast["daily"]    
-    puts "In the next #{day_number} days, temperature will be #{day["temp"]["day"]} celsius with maximum of #{day["temp"]["max"]}, minimum of #{day["temp"]["min"]} and there will be #{day["weather"][0]["description"]}"
-    day_number = day_number + 1
-end
+    #puts "Extended forecast:"
+      #  for day in @forecast["daily"]    
+      #  puts "In the next #{day_number} days, temperature will be #{day["temp"]["day"]} celsius with maximum of #{day["temp"]["max"]}, minimum of #{day["temp"]["min"]} and there will be #{day["weather"][0]["description"]}"
+       # day_number = day_number + 1
+        # end
+    view "news"
 
   ### Get the news API Key: f73238e5d6334ca982f17a6ae2c74d08
 
@@ -34,4 +33,4 @@ end
     #@news = HTTParty.get(url).parsed_response.to_hash.inspect
 # news is now a Hash you can pretty print (pp) and parse for your output
 
-# end
+ end
